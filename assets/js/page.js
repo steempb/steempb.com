6ab334d4140e1f30a0fc5af79d79d938b5248924
Nick Pettazzoni <pettazz@gmail.com>
2013-12-16 15:27:45 -0500

    add some sweet sweet smooth scrolling

--- assets/js/page.js
+++ assets/js/page.js
@@ -51,3 +51,18 @@ function suchDogeWow(){
         LIBDOGE.controller.buyDoge();
     }
 }
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});

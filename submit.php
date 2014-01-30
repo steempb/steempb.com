ce73aef83736a6e38b56d8a894d29bce378c45fe
Nick Pettazzoni <pettazz@gmail.com>
2014-01-30 16:21:40 -0500

    since we have a syrup model for this now, use it

--- submit.php
+++ submit.php
@@ -4,57 +4,24 @@
         exit();
     }
 
-    include('../steem_config.php');
-
-    /**
-     * Generates pseudo-random VALID RFC 4211 COMPLIANT Universally Unique IDentifiers (UUID) version 4.
-     * As found here: http://www.php.net/manual/en/function.uniqid.php#94959
-     * 
-     * @return string The generated UUID
-     */
-    function uuid4(){
-        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_mid"
-            mt_rand( 0, 0xffff ),
-
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand( 0, 0x0fff ) | 0x4000,
-
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand( 0, 0x3fff ) | 0x8000,
-
-            // 48 bits for "node"
-            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-        );
-    }
-
     try{
+        require('JACKED/jacked_conf.php');
+        $JACKED = new JACKED(array('Syrup'));
+
         sleep(3);
         $name = $_POST['name'];
         $email = $_POST['email'];
         $postalCode = $_POST['postalCode'];
-        $guid = uuid4();
+        $guid = $JACKED->Util->uuid4();
 
-        $obj = new mysqli($steemConfig['host'], $steemConfig['user'], $steemConfig['password'], $steemConfig['schema']);
-        if ($obj->connect_errno) {
-            throw new Exception("Connect failed: " . $obj->connect_error);
-        }
+        $inforeq = $JACKED->Syrup->InfoRequest->create();
+        $inforeq->guid = $guid;
+        $inforeq->name = $name;
+        $inforeq->email = $email;
+        $inforeq->postal_code = $postalCode;
 
-        $obj->autocommit(true);
-
-        $query = "INSERT INTO InfoRequest (`guid`, `name`, `email`, `postal_code`) VALUES ('$guid', '$name', '$email', '$postalCode')";
-        if(!$obj->query($query)){
-            throw new Exception("Error: " . $obj->error);
-        }
-
-        $obj->close();
-
+        $inforeq->save();
+        
     }catch(Exception $e){
         header('HTTP/1.1 500 Internal Server Error');
         error_log($e->getMessage());

9c364d483d734bc54bfe3029eadd8a228b649e91
Nick Pettazzoni <pettazz@gmail.com>
2014-05-10 01:08:33 -0400

    remove caching from handlebars fetch

--- assets/js/page.js
+++ assets/js/page.js
@@ -490,44 +490,13 @@ $(function() {
  *   });
  * Source: https://github.com/wycats/handlebars.js/issues/82
  */
-var Template = function() {
-    this.cached = {};
-};
+var Template = function(){};
 var T = new Template();
 $.extend(Template.prototype, {
     render: function(name, callback) {
-        if (T.isCached(name)) {
-            callback(T.cached[name]);
-        } else {
-            $.get(T.urlFor(name), function(raw) {
-                T.store(name, raw);
-                T.render(name, callback);
-            });
-        }
-    },
-    renderSync: function(name, callback) {
-        if (!T.isCached(name)) {
-            T.fetch(name);
-        }
-        T.render(name, callback);
-    },
-    prefetch: function(name) {
-        $.get(T.urlFor(name), function(raw) { 
-            T.store(name, raw);
+        $.get(T.urlFor(name), function(raw) {
+            callback(Handlebars.compile(raw));
         });
-    },
-    fetch: function(name) {
-        // synchronous, for those times when you need it.
-        if (! T.isCached(name)) {
-            var raw = $.ajax({'url': T.urlFor(name), 'async': false}).responseText;
-            T.store(name, raw);         
-        }
-    },
-    isCached: function(name) {
-        return !!T.cached[name];
-    },
-    store: function(name, raw) {
-        T.cached[name] = Handlebars.compile(raw);
     },
     urlFor: function(name) {
         return "/assets/templates/"+ name + ".handlebars";

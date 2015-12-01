165277003cab3031e5b78a0288af0b7f75c17b36
Nick Pettazzoni <pettazz@gmail.com>
2015-12-01 09:22:10 -0500

    update pricing for value meal

--- assets/js/page.js
+++ assets/js/page.js
@@ -338,7 +338,7 @@ $(document).ready(function(){
                     cart_context.item_id = productIdValueMeal;
                     cart_context.item_name = '2 Jars + T-Shirt';
                     cart_context.item_quantity = 1;
-                    cart_context.item_subtotal = 19.99;
+                    cart_context.item_subtotal = 24.99;
                     cart_context.shipping_total = 0;
                     checkoutSlide(2);
                 });
